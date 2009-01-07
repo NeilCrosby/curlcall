39aabe8441b6cdad88fed34e04134984a44200e5
Neil Crosby <neil@thetenwordreview.com>
2009-01-07 22:33:51 +0000

    Add very basic tests for getFromJsonSource and getFromPhpSource

---
+++ tests/endpoints/endpoint.php
@@ -0,0 +1,38 @@
+<?php
+
+$dataType = isset($_GET['type']) ? $_GET['type'] : 'array';
+$output   = isset($_GET['output']) ? $_GET['output'] : 'php';
+
+$array = array('some_data', 'some other data');
+$string = 'some_data';
+
+$data = null;
+switch($dataType) {
+    case 'true':
+        $data = true;
+        break;
+    case 'false':
+        $data = false;
+        break;
+    case 'null':
+        $data = null;
+        break;
+    case 'string':
+        $data = $string;
+        break;
+    default:
+        $data = $array;
+}
+
+
+switch ($output) {
+    case 'json':
+        echo json_encode($data);
+        break;
+//    case 'xml':
+//        break;
+    default:
+        echo serialize($data);
+}
+
+?>
